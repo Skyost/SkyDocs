977051ee6db693c24cc5c00ada5f22a320747d82
Skyost <me@skyost.eu>
2017-06-12 01:24:05 +0200

    Added a docs folder.

---
+++ docs/assets/js/page.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	// HANDLING TOP NAV BAR :
+	$('#navbar ul li ul').each(function(){
+		$(this).replaceWith('<div>' + $(this).html() + '</div>');
+	});
+	$('#navbar ul li div li').each(function(){
+		$(this).replaceWith($(this).html());
+	});
+	
+	$('#navbar ul li div').addClass('dropdown-menu');
+	$('#navbar ul li div a').addClass('dropdown-item');
+	
+	var currentID = 1;
+	$('#navbar ul li div').each(function() {
+		$(this).attr('aria-labelledby', 'navbarDropdownMenuLink' + currentID);
+		var parent = $(this).parent();
+		parent.addClass('dropdown')
+		parent.find('> a').addClass('dropdown-toggle').attr('id', 'navbarDropdownMenuLink' + (currentID++)).attr('data-toggle', 'dropdown').attr('aria-haspopup', 'true').attr('aria-expanded', 'false');
+	});
+	
+	$('#navbar ul').addClass('navbar-nav mr-auto');
+	$('#navbar ul li').addClass('nav-item');
+	$('#navbar ul li > a').addClass('nav-link');
+	
+	// TABLE OF CONTENTS :
+	$('#content nav').toc({
+		'container': '#content article',
+		'prefix': 'nav'
+	});
+	
+	$(window).scroll(function() {
+		if($('#content nav ul').height() > $(window).height()) {
+			return;
+		}
+		if($(window).width() < 750) {
+			$('#content nav ul').css('position', '');
+			return;
+		}
+		$('#content nav ul').css('position', isScrolledIntoView($('#navbar')) ? '' : 'fixed').css('top', '31px');
+	});
+	
+	// ANCHORS LINKS :
+	$('#content h1, #content h2, #content h3, #content h4').each(function() {
+		$(this).html($(this).html() + '<a href="#' + $(this).attr('id') + '" class="anchor"><i class="fa fa-link" aria-hidden="true"></i></a>');
+	});
+	
+	// OTHERS :
+	$('table').each(function() {
+		if(!$(this).hasClass('.table')) {
+			$(this).addClass('table table-bordered table-responsive');
+		}
+	});
+});
+
+function isScrolledIntoView(elem) {
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
+
+	var elemTop = elem.offset().top;
+	var elemBottom = elemTop + elem.height();
+
+	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
