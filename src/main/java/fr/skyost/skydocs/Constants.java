137f82e259e2c2064fc2cc8f331672323fb345ae
Skyost <me@skyost.eu>
2017-08-28 12:51:13 +0200

    This will be v0.4.

--- src/main/java/fr/skyost/skydocs/Constants.java
+++ src/main/java/fr/skyost/skydocs/Constants.java
@@ -22,7 +22,7 @@ public class Constants {
 	 * App's version.
 	 */
 	
-	public static final String APP_VERSION = "v0.3.3 Beta";
+	public static final String APP_VERSION = "v0.4 Beta";
 	
 	/**
 	 * App's authors.
