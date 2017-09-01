724714f981948e8580e2f6c1f70a0357660435fe
Skyost <me@skyost.eu>
2017-09-01 16:23:08 +0200

    Updated docs, yet another time.

--- docs/assets/localization/en.js
+++ docs/assets/localization/en.js
@@ -1,7 +1,1 @@
-$(document).ready(function() {
-	localize({
-		'search-box-placeholder': 'Search...',
-		'paginator-previous': 'Previous page',
-		'paginator-next': 'Next page'
-	});
-});
+$(document).ready(function(){localize({"search-box-placeholder":"Search...","paginator-previous":"Previous page","paginator-next":"Next page"})});
--- docs/assets/localization/fr.js
+++ docs/assets/localization/fr.js
@@ -1,7 +1,1 @@
-$(document).ready(function() {
-	localize({
-		'search-box-placeholder': 'Recherche...',
-		'paginator-previous': 'Page précédente',
-		'paginator-next': 'Page suivante'
-	});
-});
+$(document).ready(function(){localize({"search-box-placeholder":"Recherche...","paginator-previous":"Page précédente","paginator-next":"Page suivante"})});
